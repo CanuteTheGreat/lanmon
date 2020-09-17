6b23f0c2f3dc5f2cb9b6030353e295bae1b698b9
Ronald Farrer <canutethegreat@gmail.com>
2020-09-17 12:01:11 -0700

    Added example conf file.

--- lanmon.py
+++ lanmon.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
+from configparser import ConfigParser
+import psycopg2
 
 # get args
 # need network ip range (i.e. 10.0.0.0/24)
@@ -13,10 +15,41 @@ import argparse
 
 # if a mac already exists, check that it has the same ip, if not alert 
 
+lanmonversion = "0.16"
+
 def readargs():
     parser = argparse.ArgumentParser()
-    parser.parse_args()
+    parser.add_argument('-V', '--version', action='version', version='%(prog)s ' + lanmonversion)
+    parser.add_argument('-c', '--conf', '--config', action="store", dest="configfile", default='lanmon.conf')
+
+    # parse the args
+    args = parser.parse_args()
+    filename = args.configfile 
+
+    conf = lanmonconfig(filename, 'main')
+
+    print(conf)
+
+
+    quit()
     return("10.0.0.1/24") # FIXME: test
+
+def lanmonconfig(filename, section):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+
+    # get section, default to main
+    lanmoncfg = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            lanmoncfg[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+    return lanmoncfg
+
 
 def netscan(ip):
     # FIXME: finish
